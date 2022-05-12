659da0c5b1d0e9a9110ff66a181fc5a833b9918b
Pat Lawler <patlaw777@icloud.com>
2022-05-12 17:52:55 -0500

    connected front end

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -19,10 +21,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   
   server.applyMiddleware({ app });
+
+  
 
   db.once('open', () => {
     app.listen(PORT, () => {
