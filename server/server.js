5f1b67c2e795c3f125eb3ecda330d6ed02c3ba42
Pat Lawler <patlaw777@icloud.com>
2022-05-13 08:46:42 -0500

    completed 2.4

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const path = require('path');
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./schemas');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-
 const server = new ApolloServer({
-  
   typeDefs,
   resolvers,
-  context: authMiddleware
- 
+  context: authMiddleware,
 });
 
 const app = express();
@@ -30,19 +27,18 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  
   server.applyMiddleware({ app });
-
   
-
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
-};
-
-startApolloServer(typeDefs, resolvers);
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,41 +1,37 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretshhhh';
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    signToken: function({ username, email, _id }) {
-        const payload = { username, email, _id };
+  authMiddleware: function({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
+    }
 
-   authMiddleware: function({ req }) {
-  // allows token to be sent via req.body, req.query, or headers
-  let token = req.body.token || req.query.token || req.headers.authorization;
+    if (!token) {
+      return req;
+    }
 
-  // separate "Bearer" from "<tokenvalue>"
-  if (req.headers.authorization) {
-    token = token
-      .split(' ')
-      .pop()
-      .trim();
-  }
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
 
-  // if no token, return request object as is
-  if (!token) {
     return req;
-  }
+  },
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
 
-  try {
-    // decode and attach user data to request object
-    const { data } = jwt.verify(token, secret, { maxAge: expiration });
-    req.user = data;
-  } catch {
-    console.log('Invalid token');
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   }
-
-  // return updated request object
-  return req;
-}
 };
